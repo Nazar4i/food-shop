5c49377d22070be243dda5f2dd032715ff82dbde
nazar.karpyn <nazar.karpyn@yalantis.net>
2021-02-23 11:08:17 +0200

    fix routing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {Recipe} from "./pages/Recipe";
 
 function App() {
   return <>
-    <Router>
+    <Router basename="/food-shop">
       <Header />
       <main className="container content">
           <Switch>
